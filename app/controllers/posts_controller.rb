835a584ece5f9f0be3c038c5551b858cf3eeebb4
Zeon <zeonstewart23@gmail.com>
2021-10-15 12:56:48 -0400

    commit tree bed0128394acf71d72e04ae51ae73b7512fcdcda

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def show
     post = Post.find(params[:id])
     
@@ -8,13 +8,16 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
     post.update(post_params)
-
-    render json: post
+    render json: post, status: :created
+    
   end
 
   private
+
+  def render_not_found_response
+    render json: { error: "Not found" }, status: :unprocessable_entity
+  end
 
   def post_params
     params.permit(:category, :content, :title)
